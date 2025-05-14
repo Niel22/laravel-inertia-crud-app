df30ec6cfc18baee652fa1b679eeeb4babf65b4e
Niel22 <niel2264@gmail.com>
2025-05-14 10:38:47 +0100

    Project Completed

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -35,15 +35,33 @@ export default function Home({posts}){
                 <div className="absolute top-24 right-6 bg-rose-500 p-2 rounded-md shadow-lg text-sm text-white">{flashMessage.error}</div>
             )}
 
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {posts.data.map(post => (
-                    <div key={post.id} className="p-4 border-b">
-                        <div className="text-small text-slate-500">
-                            <span>Posted On: </span>
-                            <span>{new Date(post.created_at).toLocaleTimeString()}</span>
-                        </div>
-                        <p className="font-medium">{post.body}</p>
-                        <Link href={route('posts.show', post.id)} className="text-link">Read more ...</Link>
+                    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            {post.body.length > 100 ? `${post.body.slice(0, 100)}...` : post.body}
+                        </p>
+                        <Link
+                            href={route('posts.show', post.id)}
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                        Read more
+                        <svg
+                        className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
+                        >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                        </svg>
+                    </Link>
                     </div>
                 ))}
             </div>
